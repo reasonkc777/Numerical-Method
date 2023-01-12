cfba547c7421f08842c7f79a43f10bd78919e103
Nishchal K.C <81420040+reasonkc777@users.noreply.github.com>
2023-01-12 16:07:38 +0545

    Add files via upload

---
+++ Heun’s_Method.c
@@ -0,0 +1,30 @@
+//Heun’s Method
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float func(float x, float y){
+    float f;
+    f=x+y;
+    return f;
+}
+int main(){
+ int i, n;
+ float x0, y0, xp, h, m1, m2;
+ printf("Enter initial value of x and y:");
+ scanf("%f %f", &x0,&y0);
+ printf("Enter x at which y is required:");
+ scanf("%f", &xp);
+ printf("Enter stepsize,h: ");
+ scanf("%f", &h);
+ n = (xp - x0)/h;
+ for(i=1; i<=n; i++){
+    m1 = func(x0,y0);
+    m2 = func(x0+h, y0+m1*h);
+    x0 = x0+h;
+    y0 = y0+0.5*h*(m1+m2);
+    printf("%f \t%f\n", x0,y0);
+ }
+ printf("\nValue of y at x=%f is %f",x0,y0);
+ getch();
+ return 0;
+}
